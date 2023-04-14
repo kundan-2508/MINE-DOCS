e6f5150e4ad9aabe5c51f4b9151fe47071ca7184
Kundan Kumar <kun123658@gmail.com>
2023-04-15 00:19:08 +0530

    Update dbh.inc.php

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "minedocs";
+$dbServername = "minedocs";
+$dbUsername = "minedocs_admin";
+$dbPassword = "testpassword@123";
+$dbName = "";
 
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword);
 mysqli_select_db($conn,$dbName);
